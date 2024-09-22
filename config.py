3a4cfa9680c0bebd4b1a9f9a5e1b8d68f7d4be6a
T30BWM <varmanaveen002@gmail.com>
2024-09-22 15:46:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7777077126:AAGbprbWlEOYoA7z8RAIOFbW_2rTyz0i1cs")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("T30BMW")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6979469110")
+
+
+
+
+
+  
